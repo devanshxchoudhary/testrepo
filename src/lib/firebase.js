e3432ae76eeef4beaf10ba8e206763c3418a585c
Devansh Choudhary <devanshdc19@gmail.com>
2024-07-25 00:10:14 +0530

    Final commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,15 +5,15 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "convo-space-b8a0f.firebaseapp.com",
-  projectId: "convo-space-b8a0f",
-  storageBucket: "convo-space-b8a0f.appspot.com",
-  messagingSenderId: "843916434003",
-  appId: "1:843916434003:web:435e98b3db5c5da60c84a6"
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth()
-export const db = getFirestore()
-export const storage = getStorage()
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
